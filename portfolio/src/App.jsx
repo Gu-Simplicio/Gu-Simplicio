0e1cf9918b88419fccbf1d9d848ba9f54e1fca9c
Gu-Simplicio <gustavosilvasimplicio08@gmail.com>
2026-08-15 20:01:59 -0300

    feat(portfolio): initial structure of portfolio created and configured

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,9 @@
+function App(){
+  return (
+    <main className="w-screen h-screen text-center font-bold text-xl bg-red-500">  
+      <h1>Hello, World</h1>
+    </main>
+  )
+}
+
+export default App;
