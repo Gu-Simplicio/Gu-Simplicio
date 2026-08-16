d3bd8ab76a64543b9358786b5fc455465ebea400
Gu-Simplicio <gustavosilvasimplicio08@gmail.com>
2026-08-16 00:56:47 -0300

    feat(portfolio): header created to celphone screens

--- portfolio/src/components/Header/Header.jsx
+++ portfolio/src/components/Header/Header.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 function HeaderLink(props) {
     const { txt, ref } = props;
 
@@ -9,6 +11,8 @@ function HeaderLink(props) {
 }
 
 function Header(){
+    const [openNav, setOpenNav] = useState(false);
+
     return (
         <header className="fixed top-0 right-0">
             {/* PC SCREEN */}
@@ -30,9 +34,32 @@ function Header(){
             </div>
 
             {/* CELPHONE SCREEN */}
-            <div className="md:hidden flex
-                    ">
-                <h1>Tela de cll</h1>
+            <button 
+                className="absolute right-5 top-2 cursor-pointer md:hidden"
+                onClick={() => setOpenNav(!openNav)}> 
+                {openNav ? 'Fechar' : 'Abrir'}
+            </button>
+            <div className={`md:hidden flex
+                    pt-10 pl-2
+                    ${ openNav ? 'w-[7.5rem]' : 'w-[0px]' } h-screen
+                    flex-col gap-2
+                    bg-[var(--bg-color2)]
+                    transition-all`}>
+                
+                <nav>
+                    <ul className="
+                            flex flex-col gap-2">
+                        <li>
+                            <HeaderLink txt="Projetos" ref="#projetos"/>
+                        </li>
+                        <li>
+                            <HeaderLink txt="Educação" ref="#educacao"/>
+                        </li>
+                        <li>
+                            <HeaderLink txt="Contatos" ref="#contatos"/>
+                        </li>
+                    </ul>
+                </nav>
             </div>
         </header>
     );
